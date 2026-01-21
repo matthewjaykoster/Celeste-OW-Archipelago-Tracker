2746b1261173c93d66364a788c33c147789f2b59
Matt Koster <matthew.jay.koster@gmail.com>
2026-01-21 11:44:10 -0500

    Added next TODO. Fixed a small inconsitency with B and C side level clears.

--- scripts/init.lua
+++ scripts/init.lua
@@ -1,11 +1,15 @@
 ENABLE_DEBUG_LOG = true
 ENABLE_DEBUG_LOG_VERBOSE = true
 
+-- Functions
 ScriptHost:LoadScript("scripts/utils.lua")
+ScriptHost:LoadScript("scripts/logic.lua")
+
+-- JSON Loads
 ScriptHost:LoadScript("scripts/items.lua")
 ScriptHost:LoadScript("scripts/locations.lua")
 ScriptHost:LoadScript("scripts/layouts.lua")
-ScriptHost:LoadScript("scripts/logic.lua")
-ScriptHost:LoadScript("scripts/autotracking.lua")
-
 Tracker:AddMaps("maps/maps.json")
+
+-- Autotracking
+ScriptHost:LoadScript("scripts/autotracking.lua")
